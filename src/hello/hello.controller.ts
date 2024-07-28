2c4e5a8a0e85137ff346562b79e6bd7f8f1e13e2
N1GH7M4R3 <yordanbb98@gmail.com>
2024-07-28 18:50:14 -0400

    HTTP Codes

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Controller()
@@ -9,4 +9,33 @@ export class HelloController {
         console.log(request.url);
         return response.status(200).json({ message: "Hello World!" });
     }
+
+    @Get("/new")
+    @HttpCode(201)
+    somethingNew(){
+        return "Something new";
+    }
+
+
+    @Get("/error")
+    @HttpCode(HttpStatus.INTERNAL_SERVER_ERROR)
+    error(){
+        return "Error";
+    }
+
+
+    @Get("/notFound")
+    @HttpCode(HttpStatus.NOT_FOUND)
+    notFound(){
+        return "Not Found";
+    }
+
+
+    @Get("/badRequest")
+    @HttpCode(HttpStatus.BAD_REQUEST)
+    badRequest(){
+        return "Bad Request";
+    }
+
+
 }
