05e0c379b4c43fdec3750874c0626043e3bdaf4b
N1GH7M4R3 <yordanbb98@gmail.com>
2024-07-28 19:13:06 -0400

    Guards

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, HttpCode, HttpStatus, Param, ParseBoolPipe, ParseIntPipe, Query, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param, ParseBoolPipe, ParseIntPipe, Query, Req, Res, UseGuards } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { ValidateuserPipe } from './pipes/validateuser/validateuser.pipe';
+import { AuthGuard } from './guards/auth/auth.guard';
 
 @Controller()
 export class HelloController {
@@ -44,11 +45,13 @@ export class HelloController {
     }
 
     @Get('active/:status')
+    @UseGuards(AuthGuard)
     isUserActive(@Param('status', ParseBoolPipe) status: boolean) {
         return status;
     }
 
     @Get('greet')
+    @UseGuards(AuthGuard)
     greet(@Query(ValidateuserPipe) query: {name: string, age: number}) {
         return "Hello " + query.name + " your age is " + query.age;
     }
