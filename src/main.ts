aede9d53092b14d433634d812c25e8d8792fafdf
N1GH7M4R3 <yordanbb98@gmail.com>
2024-07-28 20:22:09 -0400

    Cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //Swagger
   const config = new DocumentBuilder()
   .setTitle('Nest js example')
   .setDescription('A brief description of your API')
@@ -16,9 +17,16 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  //Global Pipes
   app.useGlobalPipes(new ValidationPipe({
     whitelist : true
   }));
+
+  //Cors
+  app.enableCors(
+    // {
+    // origin: 'http://localhost:3000'}
+  ); 
 
   await app.listen(3000);
 }
